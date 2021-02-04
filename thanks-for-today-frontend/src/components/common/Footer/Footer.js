3730f2106ac3997f9fae055addd9f0580c6c1ae6
choieunji <ghk784@dgu.ac.kr>
2021-02-05 04:22:11 +0900

    Footer 이미지에서 css 로 변경

--- thanks-for-today-frontend/src/components/common/Footer/Footer.js
+++ thanks-for-today-frontend/src/components/common/Footer/Footer.js
@@ -2,6 +2,12 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './Footer.scss';
 
-const Footer = () => <div className="footer">Footer</div>;
+const Footer = () => <div className="footer">
+    <div className="footer-content">
+        <span>MAIN</span>
+        <span>TODO</span>
+        <span>CALENDAR</span>
+    </div>
+</div>;
 
 export default Footer;
