13fe0d360b3c200e9288a4c9c27ae13016801af5
seongjunChoi <csj000714@khu.ac.kr>
2021-02-05 15:21:38 +0900

    WordCloud

--- NLP/WordCloud_Var.py
+++ NLP/WordCloud_Var.py
@@ -2,8 +2,7 @@ import numpy as np
 import random   # numpy와 random, palettable은 font 색 설정
 from PIL import Image   # mask 이미지를 처리
 from wordcloud import WordCloud, STOPWORDS  # wordcloud는 이 모든 것을 이용해서 word cloud를 그릴 때 이용
-# https://jiffyclub.github.io/palettable/
-# from palettable.colorbrewer.qualitative import Dark2_8 # random color
+
 from palettable.cartocolors.sequential import BluGrn_7
 
 def color_func(word, font_size, position, orientation, random_state=None, **kwargs):
@@ -12,8 +11,6 @@ def color_func(word, font_size, position, orientation, random_state=None, **kwar
 font = "BMHANNA_11yrs_ttf"
 font_path = "%s.ttf" % font
 
-# https://pixlr.com/kr/editor/ - Translate Transparent(alpha) Image
-# https://dramahdg.tistory.com/1 - Search Transparent(alpha) Image
 icon = "Tree"
 icon_path = "%s.png" % icon
 with open("AppleCar.txt", 'rt', encoding="utf-8") as f:
@@ -25,7 +22,7 @@ mask = Image.new("RGB", icon.size, (255,255,255)) # backgraound: white(255,255,2
 mask.paste(icon,icon)
 mask = np.array(mask)
 wc = WordCloud(font_path=font_path, background_color="white", max_words=2000, mask=mask,
-               max_font_size=200, random_state=42, stopwords=stopwords, contour_width=3, contour_color='firebrick')
+               max_font_size=200, random_state=42)
 
 # generate word cloud
 wc.generate_from_text(message)
