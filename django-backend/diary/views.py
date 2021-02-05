719715cdea98df0c3e83022a391c5de02835f198
BitnaKeum <beausty23@naver.com>
2021-02-06 03:40:55 +0900

    백엔드 NLP 코드 업데이트

--- django-backend/diary/views.py
+++ django-backend/diary/views.py
@@ -28,7 +28,7 @@ def create(request):
             weather = request.POST.get("weather")
             body = request.POST.get("body")
 
-            key_sentence, keywords = Get_keyword(str(title) + ' ' + str(body))
+            keywords = Get_keyword(str(title) + ' ' + str(body))
             # keywords = '' # NLP 키워드 추출 부분 적용해야하는 부분
 
 
@@ -81,7 +81,7 @@ def update(request, pk):
             weather = request.POST.get("weather")
             body = request.POST.get("body")
 
-            key_sentence, keywords = Get_keyword(str(title) + ' ' + str(body))
+            keywords = Get_keyword(str(title) + ' ' + str(body))
 
             this = Today.objects.get(pk=pk)
             this.date = date
