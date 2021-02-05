a6ba182efde1b77f40e0c501940c0c6060a05e5b
choieunji <ghk784@dgu.ac.kr>
2021-02-05 12:53:07 +0900

    Footer 변경 및 diary 분석 탭 추가

--- thanks-for-today-frontend/src/components/Home/Home.js
+++ thanks-for-today-frontend/src/components/Home/Home.js
@@ -67,6 +67,9 @@ class Home extends Component {
                 <div className="sidehome">
                     <CustomSlider />
                 </div>
+                <div className="analysis">
+
+                </div>
             </div>
         )
     }
--- thanks-for-today-frontend/src/components/common/Footer/Footer.js
+++ thanks-for-today-frontend/src/components/common/Footer/Footer.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import leaf1 from 'images/leaf1.png';
+import leaf2 from 'images/leaf2.png';
 import './Footer.scss';
 
 const Footer = () => <div className="footer">
+    <div className="footer-img1"><img src={leaf1}></img></div>
+    <div className="footer-img2"><img src={leaf2}></img></div>
     <div className="footer-content">
-        <span>MAIN</span>
-        <span>TODO</span>
-        <span>CALENDAR</span>
+        <div className="footer-text">
+            <span className="text">MAIN</span>
+            <span className="text">TODO</span>
+            <span className="text">CALENDAR</span>
+        </div>
     </div>
 </div>;
 
